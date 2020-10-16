04ba8ec3c0681c6a3aa6dc14bc6766c74aebea12
Yuri Rech <yuri.nunes@tuntscorp.com>
2020-10-16 18:16:21 +0200

    test

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -4,8 +4,9 @@ import { getAllPostIds, getPostData } from '../../lib/posts'
 import Head from "next/head";
 import Date from "../../components/date";
 import utilStyles from '../../styles/utils.module.css';
+import { GetStaticProps, GetStaticPaths, GetServerSideProps } from 'next'
 
-export async function getStaticPaths() {
+export const getStaticPaths: GetStaticPaths = async () => {
   const paths = getAllPostIds()
   return {
     paths,
@@ -13,7 +14,7 @@ export async function getStaticPaths() {
   }
 }
 
-export async function getStaticProps({ params }) {
+export const getStaticProps: GetStaticProps = async ({ params }) => {
   const postData = await getPostData(params.id)
   return {
     props: {
