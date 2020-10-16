20a8fcb1891fa7c8b5bb9ca0f584d0c9b07ed209
Yuri Rech <yuri.nunes@tuntscorp.com>
2020-10-16 17:24:00 +0200

    preview

--- pages/index.js
+++ pages/index.js
@@ -30,6 +30,7 @@ export default function Home ({ allPostsData }) {
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
+        <h3>HI THERE IMMA INTRUDER</h3>
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
