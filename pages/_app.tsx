5bf565d311c351be1c06e2cb74faded6e1adb850
Yuri Rech <yuri.nunes@tuntscorp.com>
2020-10-16 18:28:32 +0200

    ts

---
+++ pages/_app.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import '../styles/global.css';
+import { AppProps } from 'next/app'
+
+function App({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />
+}
+
+export default App;
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,33 +4,31 @@ import utilStyles from '../styles/utils.module.css'
 import { getSortedPostsData } from '../lib/posts'
 import Link from 'next/link'
 import Date from '../components/date'
-import React from "react";
+import { GetStaticProps } from 'next'
 
-export async function getStaticProps() {
-  const allPostsData = getSortedPostsData()
-  return {
-    props: {
-      allPostsData
-    }
-  }
-}
-
-export default function Home ({ allPostsData }) {
+export default function Home({
+                               allPostsData
+                             }: {
+  allPostsData: {
+    date: string
+    title: string
+    id: string
+  }[]
+}) {
   return (
     <Layout home>
       <Head>
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium aliquam debitis ducimus esse inventore laborum laudantium, nobis odit quae sapiente.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet, labore, minus. Ea illo illum modi molestias mollitia non obcaecati, sunt?</p>
         <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
+          (This is a sample website - you’ll be building a site like this in{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
-        <h3>HI THERE IMMA INTRUDER</h3>
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
@@ -46,5 +44,14 @@ export default function Home ({ allPostsData }) {
         </ul>
       </section>
     </Layout>
-  );
+  )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const allPostsData = getSortedPostsData()
+  return {
+    props: {
+      allPostsData
+    }
+  }
 }
