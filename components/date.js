f6705ce6c1c3221b3edaaaa9214f014be4ce0236
Yuri Rech <yuri.nunes@tuntscorp.com>
2020-10-16 16:45:42 +0200

    first

---
+++ components/date.js
@@ -0,0 +1,7 @@
+import { parseISO, format } from 'date-fns'
+import React from "react";
+
+export default function Date({ dateString }) {
+  const date = parseISO(dateString)
+  return <time dateTime={dateString}>{format(date, 'LLLL d, yyyy')}</time>
+}
